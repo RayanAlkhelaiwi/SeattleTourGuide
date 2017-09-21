6c13431c452a675baf93cb020e1d16e81b53bc13
RayanAlkhelaiwi <r.n.m.k@live.com>
2017-09-21 22:10:39 +0300

    Change the transition to use onClickListener() and converted fragments to classes

--- app/src/main/java/com/example/android/seattletourguide/ActivitiesCategory.java
+++ app/src/main/java/com/example/android/seattletourguide/ActivitiesCategory.java
@@ -52,16 +52,16 @@ public class ActivitiesCategory extends AppCompatActivity {
 
         final ArrayList<CityAttraction> cityAttractions = new ArrayList<>();
 
-        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM",0, R.mipmap.ic_launcher));
+        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity B", "2233 Nametwo Street", "Monday - Friday", "9 AM - 8:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity C", "3344 Namethree Street", "Monday - Saturday", "11 AM - 6:30 PM", 0, R.mipmap.ic_launcher));
-        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM",0, R.mipmap.ic_launcher));
+        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity B", "2233 Nametwo Street", "Monday - Friday", "9 AM - 8:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity C", "3344 Namethree Street", "Monday - Saturday", "11 AM - 6:30 PM", 0, R.mipmap.ic_launcher));
-        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM",0, R.mipmap.ic_launcher));
+        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity B", "2233 Nametwo Street", "Monday - Friday", "9 AM - 8:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity C", "3344 Namethree Street", "Monday - Saturday", "11 AM - 6:30 PM", 0, R.mipmap.ic_launcher));
-        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM",0, R.mipmap.ic_launcher));
+        cityAttractions.add(new CityAttraction("Activity A", "1122 Nameone Street", "Saturday - Wednesday", "10:30 AM - 11:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity B", "2233 Nametwo Street", "Monday - Friday", "9 AM - 8:30 PM", 0, R.mipmap.ic_launcher));
         cityAttractions.add(new CityAttraction("Activity C", "3344 Namethree Street", "Monday - Saturday", "11 AM - 6:30 PM", 0, R.mipmap.ic_launcher));
 
